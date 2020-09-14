b17f6f7271d2917d68dcd4c06a1ff1ce2316f6a4
eilon1996 <toledanoeilon3@gmail.com>
2020-09-14 15:17:44 +0300

    first

---
+++ pages/posts/[id].js
@@ -0,0 +1,44 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import utilStyles from '../../styles/utils.module.css'
+
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+        <Head>
+        <title>{postData.title}</title>
+        </Head>
+        
+        <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+    </Layout>
+  )
+}
+
+// for more example of getStaticPaths & getStaticProps look in the bottom of the link
+// https://nextjs.org/learn/basics/dynamic-routes/polishing-index-page
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+      paths,
+      fallback: false
+    }
+}
+  
+export async function getStaticProps({ params }) {
+    
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
+  
